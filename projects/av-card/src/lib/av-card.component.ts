12a9048af1f48618368fb34b17238ca0c8e9adf6
Vladimír Antoš <vladimirantos@seznam.cz>
2021-11-07 14:22:02 +0100

    prepared component

--- projects/av-card/src/lib/av-card.component.ts
+++ projects/av-card/src/lib/av-card.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ContentChild, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'av-card',
-  template: `
-    <p>
-      av-card works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './av-card.component.html',
+  styleUrls: ['./av-card.component.scss'],
 })
 export class AvCardComponent implements OnInit {
+  @Input() name: string;
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() size: 'w-75' | 'w-50' | 'w-25';
+  @Input() collapsible: boolean;
+  @Input() isCollapse: boolean;
+  /**
+   * Pokud je true, bude možné kartu sbalit kliknutím na hlavičku karty. Při false se sbalí jen kliknutím na ikonu sbalení.
+   */
+  @Input() collapseUsingHeader: boolean = true;
 
-  constructor() { }
+  /**
+   * Zobrazení badge vedle nadpisu
+   */
+  @Input() badge: string;
+  @ContentChild('body', { static: true }) bodyTemplate: TemplateRef<any>;
+  @ContentChild('header', { static: true }) headerTemplate: TemplateRef<any>;
+  @ContentChild('headerRight', {static: true}) headerRightTemplate: TemplateRef<any>;
+  @ContentChild('preview', {static: true}) previewTemplate: TemplateRef<any>;
+  @ContentChild('subtitle', {static: true}) subtitleTemplate: TemplateRef<any>;
+  @ContentChild('footer', {static: true}) footerTemplate: TemplateRef<any>;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
